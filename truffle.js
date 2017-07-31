7a82c7f1ca18dc1c12a807d9c7e5786236532843
Peter Petrovics <szerintedmi@gmail.com>
2017-07-31 10:47:21 +0100

    truffle migrate temp fix

--- truffle.js
+++ truffle.js
@@ -2,15 +2,18 @@
 require('babel-register')
 
 module.exports = {
-  contracts_build_directory: './src/contractsBuild',
-  working_directory: './contracts',
-  //migrations_directory: './migrations',
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      gas: 4707806
+    /* TODO: tempfix because of issue: https://github.com/trufflesuite/truffle-migrate/issues/10
+          for now ./build/contracts need to be copied manually to src/contractsBuild folder after every truffle migrate
+    */
+    //contracts_build_directory: './src/contractsBuild',
+    //working_directory: './contracts',
+    //migrations_directory: './migrations',
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*', // Match any network id
+            gas: 4707806
+        }
     }
-  }
 }
