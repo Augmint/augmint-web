8a149628aa5ee98453b9b4a6c6a380fff952c7a8
Peter Petrovics <szerintedmi@gmail.com>
2017-08-01 18:37:07 +0100

    placeholders on main page for myacc & myloans

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import {Grid, Row, Col, Jumbotron} from 'react-bootstrap'
+import {Grid, Row, Col, Jumbotron, Panel} from 'react-bootstrap'
+
+const myAccountTitle = ( <h3>My UCD Account</h3> );
+const myLoanTitle = ( <h3>My UCD Loans</h3> );
 
 export default () => (
     <Grid>
@@ -10,6 +13,20 @@ export default () => (
                     <p>Hello UCD!</p>
                     <p>UCD is ...</p>
                 </Jumbotron>
+            </Col>
+        </Row>
+
+        <Row>
+            <Col xs={8} md={8}>
+                <Panel header={myAccountTitle}>
+                    <p>TODO</p>
+                </Panel>
+            </Col>
+
+            <Col xs={4} md={4}>
+                <Panel header={myLoanTitle}>
+                    <p>TODO</p>
+                </Panel>
             </Col>
         </Row>
     </Grid>
