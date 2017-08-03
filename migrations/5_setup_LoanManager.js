6ce65db57a59cbf9762fed41192ebaf0a3acef72
Peter Petrovics <szerintedmi@gmail.com>
2017-08-03 18:08:27 +0100

    added longer term loan products for tests

--- migrations/5_setup_LoanManager.js
+++ migrations/5_setup_LoanManager.js
@@ -12,8 +12,14 @@ module.exports = function(deployer, network) {
             LoanManager.deployed()
             .then( res => {
                 // term (in sec), discountRate, loanCoverageRatio, minDisbursedAmountInUcd (w/ 4 decimals), gracePerdio, isActive
-                res.addProduct( 1, 950000, 800000, 200000, 1, true);
-                res.addProduct(60, 900000, 600000, 100000, 60, true);
+                res.addProduct(31536000 /* 365d */, 800000, 800000, 300000, 864000, true);
+                res.addProduct(15552000 /* 180d */, 850000, 800000, 300000, 259200, true);
+                res.addProduct( 7776000 /*  90d */, 910000, 800000, 300000, 172800, true);
+                res.addProduct( 2592000 /*  30d */, 950000, 800000, 300000,  86400, true);
+                res.addProduct(   86400 /*   1d */, 970000, 850000, 300000,   3600, true);
+                res.addProduct(     600 /*  10m */, 980000, 900000, 300000,    600, true); // for testing
+                res.addProduct(      60 /*   1m */, 985000, 900000, 200000,     60, true); // 1 min for testing
+                res.addProduct(       1 /*   1s */, 990000, 950000, 100000,      1, true); // 1 sec for testing
             });
         }
     });
