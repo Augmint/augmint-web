088fb13e2866f11715f60f7a4c184b1e1c8554ab
Peter Petrovics <szerintedmi@gmail.com>
2017-08-04 10:07:41 +0100

    test products improvement

--- migrations/5_setup_LoanManager.js
+++ migrations/5_setup_LoanManager.js
@@ -17,9 +17,8 @@ module.exports = function(deployer, network) {
                 res.addProduct( 7776000 /*  90d */, 910000, 800000, 300000, 172800, true);
                 res.addProduct( 2592000 /*  30d */, 950000, 800000, 300000,  86400, true);
                 res.addProduct(   86400 /*   1d */, 970000, 850000, 300000,   3600, true);
-                res.addProduct(     600 /*  10m */, 980000, 900000, 300000,    600, true); // for testing
-                res.addProduct(      60 /*   1m */, 985000, 900000, 200000,     60, true); // 1 min for testing
-                res.addProduct(       1 /*   1s */, 990000, 950000, 100000,      1, true); // 1 sec for testing
+                res.addProduct(       1 /*   1m */, 985000, 900000, 200000,   3600, true); // due in 1 sec, repay in 1hr for testing
+                res.addProduct(       1 /*   1s */, 990000, 950000, 100000,      1, true); // defaults in 2 secs for testing
             });
         }
     });
