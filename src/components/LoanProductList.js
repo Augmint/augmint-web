7ae67787be86b30603337fa397253faf6f841661
Peter Petrovics <szerintedmi@gmail.com>
2017-08-04 07:40:04 +0100

    handle no filter for LoanProductList component

--- src/components/LoanProductList.js
+++ src/components/LoanProductList.js
@@ -6,7 +6,7 @@ export default function LoanProductList(props) {
     let products = props.products;
     // TODO: this causing a warning, table nested in <p>. Bootstrap has class for table within panel...
     const listItems = products == null ? <p>Loading products...</p> :
-        products.filter( props.filter).map( (prod, index) =>
+        products.filter( props.filter ? props.filter : () => {return true}).map( (prod, index) =>
                 <LoanProductDetails key={`loanProdDiv-${prod.id}`}
                     product={prod} selectComponent={props.selectComponent}/>
     );
