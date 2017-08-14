ecb61215b7e9adb4931fc1d47c3a55525ce33c14
Peter Petrovics <szerintedmi@gmail.com>
2017-08-14 21:55:18 +0100

    my account page tweaks

--- src/containers/account/index.js
+++ src/containers/account/index.js
@@ -11,24 +11,23 @@ class AccountHome extends React.Component {
             <Grid>
                 <Row>
                     <Col>
-                        <Row>
-                            <Col>
-                                <PageHeader>My Account</PageHeader>
-                            </Col>
-                        </Row>
+                        <PageHeader>My Account</PageHeader>
                     </Col>
                 </Row>
 
                 <Row>
-                    <Col xs={6} md={6}>
+                    <Col xs={6}>
                         <Col xs={12}>
-                            <AccountInfo account={this.props.userAccount} />
+                            <AccountInfo
+                                account={this.props.userAccount}
+                                header={<h3>Overview</h3>}
+                            />
                         </Col>
                         <Col xs={12}>
                             <UcdTransferForm />
                         </Col>
                     </Col>
-                    <Col xs={6} md={6}>
+                    <Col xs={6}>
                         <LoanList
                             header={<h3>My UCD Loans</h3>}
                             noItemMessage={<span>You have no loans</span>}
