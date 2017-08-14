48d4c4d56558fbc3e1aa4a7dc7cdca328f122758
Peter Petrovics <szerintedmi@gmail.com>
2017-08-14 21:38:15 +0100

    added link to loan details page from newloansuccess

--- src/containers/loan/newLoan/NewLoanSuccess.js
+++ src/containers/loan/newLoan/NewLoanSuccess.js
@@ -18,19 +18,24 @@ export default function NewLoanSuccess(props) {
             dismissable={false}
         >
             <p>
-                Your loan contract address: {address}
+                Loan id: {loanId}
+            </p>
+            <p>
+                Contract address: {address}
             </p>
             <p>
                 Disbursed: {disbursedLoanInUcd} UCD
             </p>
             <p>Don't forget to pay it back on maturity.</p>
             <p>
-                You can always check your loan's status on {" "}
+                You can always check your loans'' status on {" "}
                 <Link to="/account">My account page</Link>
+                <br />
+                or directly on {" "}
+                <Link to={"/loan/" + loanId}>this loan's page</Link>
             </p>
             <p>
-                Loan Id: {loanId} | Product id: {productId} | borrower:{" "}
-                {borrower}
+                Product id: {productId} | borrower: {borrower}
             </p>
         </EthSubmissionSuccessPanel>
     );
