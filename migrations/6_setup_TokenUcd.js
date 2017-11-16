f46c8b862f975f8511c55350e1faf9b5e02b908b
Peter Petrovics <szerintedmi@gmail.com>
2017-11-16 21:09:22 +0000

    migration scripts error handling

--- migrations/6_setup_TokenUcd.js
+++ migrations/6_setup_TokenUcd.js
@@ -1,8 +1,12 @@
 var TokenUcd = artifacts.require("./TokenUcd.sol");
 var LoanManager = artifacts.require("./LoanManager.sol");
 
-module.exports = function(deployer, network) {
-    TokenUcd.deployed().then(tokenUcd => {
-        tokenUcd.setLoanManagerAddress(LoanManager.address);
-    });
+module.exports = async function(deployer, network) {
+    try {
+        let tokenUcd = await TokenUcd.deployed();
+        return await tokenUcd.setLoanManagerAddress(LoanManager.address);
+    } catch (error) {
+        console.log("error in setup_TokenUcd", error);
+        return Promise.reject(error);
+    }
 };
--- migrations/7_deploy_Exchange.js
+++ migrations/7_deploy_Exchange.js
@@ -4,12 +4,15 @@ var SafeMath = artifacts.require("./SafeMath.sol");
 var OrdersLib = artifacts.require("./OrdersLib.sol");
 var Exchange = artifacts.require("./Exchange.sol");
 
-module.exports = function(deployer, network) {
-    deployer.link(SafeMath, Exchange);
-    deployer.link(OrdersLib, Exchange);
-    deployer.deploy(Exchange, TokenUcd.address, Rates.address).then(res => {
-        TokenUcd.deployed().then(res => {
-            res.setExchangeAddress(Exchange.address);
-        });
-    });
+module.exports = async function(deployer, network) {
+    try {
+        deployer.link(SafeMath, Exchange);
+        deployer.link(OrdersLib, Exchange);
+        await deployer.deploy(Exchange, TokenUcd.address, Rates.address);
+        tokenUcd = await TokenUcd.deployed();
+        return await tokenUcd.setExchangeAddress(Exchange.address);
+    } catch (error) {
+        console.log("error in deploy_Exchange", error);
+        return Promise.reject(error);
+    }
 };
