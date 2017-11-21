63814336b00ee857850159ec477db747d3b3e917
Peter Petrovics <szerintedmi@gmail.com>
2017-11-21 15:52:49 +0000

    deploy to ropsten+rinkeby

--- src/modules/ethereum/gas.js
+++ src/modules/ethereum/gas.js
@@ -1,10 +1,10 @@
 export const cost = {
     NEW_LOAN_GAS: 1000000, // As of now it's on testRPC: first= 865435 - 892077 additional = 835435 - 847058
-    NEW_FIRST_LOAN_GAS: 1000000,
-    REPAY_GAS: 130000, // as of now on testRpc: 113188, 2nd from same AC: ?
+    NEW_FIRST_LOAN_GAS: 1000000, // on ropsten: 892077
+    REPAY_GAS: 150000, // as of now on testRpc: 113188, 2nd from same AC: ?. on rinkeby: 126251
     COLLECT_GAS: 130000, // as of now on testRpc: 89419
 
-    TRANSFER_UCD_GAS: 100000, // on testrpc: first: 75189 - 75405
+    TRANSFER_UCD_GAS: 100000, // on testrpc: first: 75189 - 75405, rinkeby first: 76629
     //consecutive : no narr: 45405 - 60405 (higher when sent to account which never received)
     // w narrative: 46733 - 56693
 
