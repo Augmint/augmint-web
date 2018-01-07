9f94b6dadabd6b15d88aa0f0d5135c0e482a1ab7
Peter Petrovics <szerintedmi@gmail.com>
2018-01-07 14:53:36 +0000

    test loan product data fix

--- migrations/6_deploy_LoanManager.js
+++ migrations/6_deploy_LoanManager.js
@@ -38,7 +38,7 @@ module.exports = function(deployer, network, accounts) {
         await lm.addProduct(7776000, 910000, 800000, 300000, true); // due in 90d
         await lm.addProduct(2592000, 950000, 800000, 300000, true); // due in 30d
         await lm.addProduct(86400, 970000, 850000, 300000, true); // due in 1 day
-        await lm.addProduct(1, 985000, 900000, 200000, true); // due in 1hr for testing repayments
+        await lm.addProduct(3600, 985000, 900000, 200000, true); // due in 1hr for testing repayments
         await lm.addProduct(1, 990000, 950000, 100000, true); // defaults in 1 secs for testing
     });
 };
