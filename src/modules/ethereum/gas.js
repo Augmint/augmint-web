0c995ee124dfba583b96127f9416b25238e8715f
Peter Petrovics <szerintedmi@gmail.com>
2018-01-11 11:39:16 +0000

    gas cost fix

--- src/modules/ethereum/gas.js
+++ src/modules/ethereum/gas.js
@@ -1,8 +1,8 @@
 export const cost = {
-    NEW_LOAN_GAS: 320000, // As of now it's on testRPC: first for account = 296983  additional =  287467
+    NEW_LOAN_GAS: 350000, // As of now it's on testRPC: first for account = 296983  additional =  287467 first for user: testrpc: 320528
     NEW_FIRST_LOAN_GAS: 3600000, // on test rpc: 347467 rinkeby: ??
     REPAY_GAS: 1300000, // AugmintToken.repayLoan, as of now on testRpc: 108373 - 108461, 2nd from same AC: ?. on rinkeby: ???
-    COLLECT_BASE_GAS: 90000, // as of now on testRpc: 1 loan = 89744
+    COLLECT_BASE_GAS: 90000, // as of now on testRpc: 1 loan = first: 72639, 2nd: 63989
     COLLECT_ONE_GAS: 40000, // as of now: ???
 
     TRANSFER_AUGMINT_TOKEN_GAS: 100000, // on testrpc: first: 75189 - 75405, rinkeby first: 76629
