d00e4e498e2eef38e077dfd832475a36369e6c27
Peter Petrovics <szerintedmi@gmail.com>
2018-01-11 19:52:18 +0000

    hide web3 XHR logs from cypress test UI

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,22 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+const defaultWhitelist = xhr => {
+    // this function receives the xhr object in question and
+    // will whitelist if it's a GET that appears to be a static resource
+    return xhr.method === "GET" && /\.(jsx?|html|css)(\?.*)?$/.test(xhr.url);
+};
+
+Cypress.Server.defaults({
+    whitelist: xhr => {
+        // hide logging web3 polling XHR request to testrpc
+        return (
+            (xhr.method === "POST" && xhr.url === "http://localhost:8545/") ||
+            (xhr.method === "GET" &&
+                xhr.url.slice(0, 34) ===
+                    "http://localhost:3000/sockjs-node/") ||
+            defaultWhitelist(xhr)
+        );
+    }
+});
