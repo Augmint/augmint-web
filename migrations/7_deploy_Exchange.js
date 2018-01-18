9f07183153185b5be1410a7dcafe08f266b4c2dd
Peter Petrovics <szerintedmi@gmail.com>
2018-01-18 14:10:32 +0000

    removed old OrdersLib

--- migrations/7_deploy_Exchange.js
+++ migrations/7_deploy_Exchange.js
@@ -1,12 +1,10 @@
 const TokenAce = artifacts.require("./TokenAce.sol");
 const Rates = artifacts.require("./Rates.sol");
 const SafeMath = artifacts.require("./SafeMath.sol");
-const OrdersLib = artifacts.require("./OrdersLib.sol");
 const Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer, network) {
     deployer.link(SafeMath, Exchange);
-    deployer.link(OrdersLib, Exchange);
     deployer.deploy(Exchange, TokenAce.address, Rates.address, 100);
     deployer.then(async () => {
         const tokenAce = TokenAce.at(TokenAce.address);
