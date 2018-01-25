b3a0443973fdeaacaf879e0bd2cfe102fafa56b5
Laszlo Heves <laszlo.heves@festicket.com>
2018-01-25 10:05:14 +0000

    not connectedHome refactor baby steps

--- src/containers/home/NotConnectedHome.js
+++ src/containers/home/NotConnectedHome.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { Hero } from "./Hero";
-import { DcmOverview } from "./DcmOverview";
-import { ProjectStatus } from "./ProjectStatus";
+import { Header, Container, Divider, Image, Grid } from "semantic-ui-react";
 
 export default class NotConnectedHome extends React.Component {
     render() {
         return (
-            <div>
-                <Hero />
-                <DcmOverview />
-                <ProjectStatus />
-            </div>
+            <Container as="article">
+                <Container as="heading">
+                    <Header textAlign="center" as="h2">
+                        Augmint offers digital tokens (A-Euro) pegged to a fiat currency. Stored securely in a decentralised way, stable crypto tokens are instantly transferable worldwide.
+                    </Header>
+                </Container>
+                <Divider hidden />
+                <Container textAlign="center" as="section">
+                    <Image centered>
+                        <img src="http://placehold.it/400x200" />
+                    </Image>
+                </Container>
+                <Divider hidden />
+            </Container>
         );
     }
 }
