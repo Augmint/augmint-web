aa4ca88febfc88d6853136c74a3cf74a6c6d122f
Laszlo Heves <laszlo.heves@festicket.com>
2018-01-25 10:04:32 +0000

    refactor app menu

--- src/containers/app/AppMenu.js
+++ src/containers/app/AppMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Segment, Container, Menu, Dropdown } from "semantic-ui-react";
+import { Menu, Dropdown } from "semantic-ui-react";
 import { Link, NavLink } from "react-router-dom";
 
 export function AppMenu(props) {
@@ -14,64 +14,64 @@ export function AppMenu(props) {
         connectionStatus = "not connected";
     }
     return (
-        <Segment inverted style={{ margin: 0 }}>
-            <Menu inverted pointing secondary stackable size="large">
-                <Container>
-                    <Menu.Item active={location.pathname === "/"} as={Link} to="/">
-                        Home
+        <Menu size="large">
+                <Menu.Item
+                    active={location.pathname === "/"}
+                    as={Link}
+                    to="/"
+                >
+                    Home
+                </Menu.Item>
+
+                <Menu.Item
+                    active={location.pathname === "/concept"}
+                    as={Link}
+                    to="/concept"
+                >
+                    Concept
+                </Menu.Item>
+
+                {isConnected && (
+                    <Menu.Item as={NavLink} to="/account">
+                        My Account
+                    </Menu.Item>
+                )}
+                {isConnected && (
+                    <Menu.Item as={NavLink} to="/exchange">
+                        Buy/Sell ACD
+                    </Menu.Item>
+                )}
+                {isConnected && (
+                    <Menu.Item as={NavLink} to="/loan/new">
+                        Get ACD Loan
+                    </Menu.Item>
+                )}
+                {isConnected && (
+                    <Menu.Item as={NavLink} to="/reserves">
+                        Reserves
+                    </Menu.Item>
+                )}
+
+                <Menu.Menu position="right">
+                    <Menu.Item as={NavLink} to="/faq">
+                        FAQ
                     </Menu.Item>
 
-                    <Menu.Item active={location.pathname === "/concept"} as={Link} to="/concept">
-                        Concept
+                    <Menu.Item>
+                        <small>
+                            <Dropdown text={connectionStatus}>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item
+                                        icon="settings"
+                                        as={NavLink}
+                                        to="/under-the-hood"
+                                        text="Under the hood"
+                                    />
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </small>
                     </Menu.Item>
-
-                    <Menu.Item active={location.pathname === "/tryit"} as={Link} to="/tryit">
-                        Try it
-                    </Menu.Item>
-
-                    {isConnected && (
-                        <Menu.Item as={NavLink} to="/account">
-                            My Account
-                        </Menu.Item>
-                    )}
-                    {isConnected && (
-                        <Menu.Item as={NavLink} to="/exchange">
-                            Buy/Sell A-EUR
-                        </Menu.Item>
-                    )}
-                    {isConnected && (
-                        <Menu.Item as={NavLink} to="/loan/new">
-                            Get A-EUR Loan
-                        </Menu.Item>
-                    )}
-                    {isConnected && (
-                        <Menu.Item as={NavLink} to="/reserves">
-                            Reserves
-                        </Menu.Item>
-                    )}
-
-                    <Menu.Menu position="right">
-                        <Menu.Item as={NavLink} to="/aboutUs">
-                            About Us
-                        </Menu.Item>
-
-                        <Menu.Item>
-                            <small>
-                                <Dropdown text={connectionStatus}>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item
-                                            icon="settings"
-                                            as={NavLink}
-                                            to="/under-the-hood"
-                                            text="Under the hood"
-                                        />
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </small>
-                        </Menu.Item>
-                    </Menu.Menu>
-                </Container>
-            </Menu>
-        </Segment>
+                </Menu.Menu>
+        </Menu>
     );
 }
