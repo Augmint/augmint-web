45afff3dd61e65ad1368bb9219e33e2cff57cebf
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-01-26 20:55:32 +0100

    [footer] second step (add some new style and slack logo)

--- src/containers/app/AppFooter.js
+++ src/containers/app/AppFooter.js
@@ -1,42 +1,67 @@
 import React from "react";
-import { Segment, Container, List } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Segment, Container, List, Image } from "semantic-ui-react";
 import backgroundImg from "assets/images/globe.png";
+import slackLogo from "assets/images/slack-icon.svg";
 
-const style = {
+const segmentStyle = {
   background: 'url(' + backgroundImg + ') no-repeat center',
   height: '100%',
-  paddingTop: '353px',
+  paddingTop: '333px',
   position: 'absolute',
   width: '100%',
 }
 
+const footerTitleStyle = {
+  color: '#fff',
+  fontSize: 13,
+  letterSpacing: 2.6,
+  margin: 0,
+}
+
 const descriptionStyle = {
+  color: '#fff',
+  fontSize: 15,
+  lineHeight: 1.47,
   margin: 'auto',
   marginBottom: 30,
+  opacity: '0.7',
   padding: 0,
   maxWidth: 397,
 }
 
+const slackStyle = {
+  color: '#ffad00',
+  fontSize: 17,
+  lineHeight: 1.29,
+}
+
+const slackImg = <Image src={slackLogo} as="a" size="tiny" href="http://decent-slack-autoinvite.herokuapp.com/" target="_blank" />;
 
 export function AppFooter(props) {
     let community = "WE'RE AN ACTIVE COMUNITY",
         description = "We're a group of developers, entrepeneurs, economists and technologists. We have an open governance stucture - anyone ca join at any time.";
 
     return (
-        <Segment basic textAlign="center" style={style}>
+        <Segment basic textAlign="center" style={segmentStyle}>
             <Container fluid>
-                <Segment basic style={{margin: 0}}>
+                <Segment basic style={footerTitleStyle}>
                   {community}
                 </Segment>
-                <Segment basic size="small" style={descriptionStyle}>
+                <Segment basic style={descriptionStyle}>
                   {description}
                 </Segment>
+                <Segment basic style={slackStyle}>
+                  {slackImg}
+                  Join our Slack chanel
+                </Segment>
+
                 <List horizontal inverted divided link>
-                    <List.Item as='a' href="https://github.com/DecentLabs/dcm-poc" content="GITHUB" />
-                    <List.Item as='a' href="https://gitter.im/" content="GITTER" />
-                    <List.Item as='a' href="https://decent-team.slack.com" content="SLACK" />
-                    <List.Item as='a' href="https://docs.google.com/document/d/1IQwGEsImpAv2Nlz5IgU_iCJkEqlM2VUHf5SFkcvb80A/edit" content="WHITEPAPER" />
-                    <List.Item as='a' href="http://www.decent.org/" content="BLOG" />
+                    <List.Item as='a' href="https://github.com/DecentLabs/dcm-poc" target="_blank" content="GITHUB" />
+                    <List.Item as='a' href="https://gitter.im/" target="_blank" content="GITTER" />
+                    <List.Item as='a' href="https://decent-team.slack.com" target="_blank" content="SLACK" />
+                    <List.Item as='a' href="https://docs.google.com/document/d/1IQwGEsImpAv2Nlz5IgU_iCJkEqlM2VUHf5SFkcvb80A/edit" target="_blank" content="WHITEPAPER" />
+                    <List.Item as='a' href="http://www.decent.org/" target="_blank" content="BLOG" />
                 </List>
             </Container>
         </Segment>
