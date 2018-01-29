3607f3dcfd47d32a728fe198453d8ac2298260e3
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-01-30 00:56:03 +0100

    [footer] remove blog menu option

--- src/containers/app/AppFooter.js
+++ src/containers/app/AppFooter.js
@@ -34,7 +34,6 @@ export function AppFooter(props) {
                     <List.Item as='a' href="https://gitter.im/digital-credit-money/Lobby?utm_source=ProjectStatus&utm_medium=web&utm_campaign=init" target="_blank" content="GITTER" />
                     <List.Item as='a' href="https://decent-team.slack.com/messages/C7J38BUM6/" target="_blank" content="SLACK" />
                     <List.Item as='a' href="https://docs.google.com/document/d/1IQwGEsImpAv2Nlz5IgU_iCJkEqlM2VUHf5SFkcvb80A/edit" target="_blank" content="WHITEPAPER" />
-                    <List.Item as='a' href="http://www.decent.org/" target="_blank" content="BLOG" />
                 </List>
                 <Subscribe />
             </Container>
