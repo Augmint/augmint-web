f00a2137c797cc7e64a6099e0b25fe97d3e35e30
Peter Petrovics <szerintedmi@gmail.com>
2018-01-31 10:19:38 +0100

    build warning fixes

--- src/containers/home/concept/index.js
+++ src/containers/home/concept/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Tsegment, Tblock } from "components/TextContent";
 import { Link } from "react-router-dom";
 import { Button, List, Segment, Image, Grid } from "semantic-ui-react";
 
