6c5f3a30346402ae9a39ead0899ca98bcc2ace2d
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-02-13 14:57:10 +0100

    [tryit] add disclaimer link to the trait page

---
+++ src/containers/home/tryIt/Disclaimer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export function Disclaimer(props) {
+    return (
+        <div style={{ margin: "auto" }}>
+            <p>
+                'The tokens issued by Augmint contracts are not legal tender. Use them at your own risk...'
+                <br />
+                <Link to="/disclaimer">
+                 For more details click here.
+              </Link>
+            </p>
+        </div>
+    );
+}
