bc68aa7d091fd3f2f7ad3ec18143607466fb5456
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-02-15 19:50:05 +0100

    [discord_button] change style (hotfix)

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "semantic-ui-react";
+import { Segment, Image } from "semantic-ui-react";
+
+import discordSVG from "assets/images/Discord-Logo.svg";
 
 export function DiscordButton(props) {
-    const {
-        content = "Ask for help on our Discord",
-        icon = "chat",
-        labelPosition = "left",
-        size = "large",
-        target = "_blank",
-        primary = true,
-        ...other
-    } = props;
     return (
-        <Button
-            as={Link}
-            to="https://discordapp.com/invite/PwDmsnu"
-            icon={icon}
-            labelPosition={labelPosition}
-            content={content}
-            target={target}
-            size={size}
-            primary={primary}
-            {...other}
-        />
+        <Segment
+            basic
+            className="discord"
+            size="small"
+            as="a"
+            href="https://discord.gg/PwDmsnu"
+            target="_blank"
+            style={{ color: "#ffad00", display: "block", fontSize: 18, lineHeight: 1.29, margin: 0, padding: 0 }}
+        >
+            <Image src={discordSVG} style={{ display: "inline-block", marginRight: 20, maxWidth: 40 }}/>
+            Ask for help on our Discord
+        </Segment>
     );
 }
