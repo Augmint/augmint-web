927a67033e37f1ed035ab87883f6fc6d02322988
Peter Petrovics <szerintedmi@gmail.com>
2018-02-15 02:42:57 +0000

    netlify dotenv for underthehood

--- src/containers/underthehood/components/SystemInfo.js
+++ src/containers/underthehood/components/SystemInfo.js
@@ -7,35 +7,35 @@ export function SystemInfo(props) {
             <p>NODE_ENV: {process.env.NODE_ENV} </p>
 
             {/*netlify env variables, see: https://www.netlify.com/docs/continuous-deployment/#build-environment-variables */}
-            <p>Netlify env variables: </p>
+            <p>Netlify env variables:</p>
             <ul>
                 <li>
-                    REPOSITORY_URL: {process.env.REPOSITORY_URL}
+                    REPOSITORY_URL: {process.env.REACT_APP_REPOSITORY_URL}
                     <br />URL to the Git repository the build pulls changes from.
                 </li>
                 <li>
-                    BRANCH: {process.env.BRANCH}
+                    BRANCH: {process.env.REACT_APP_BRANCH}
                     <br />Reference to check out after fetching changes from the Git repository.
                 </li>
                 <li>
-                    PULL_REQUEST: {process.env.PULL_REQUEST}
+                    PULL_REQUEST: {process.env.REACT_APP_PULL_REQUEST}
                     <br />Whether the build is from a pull request or not.
                 </li>
                 <li>
-                    HEAD: {process.env.HEAD}
+                    HEAD: {process.env.REACT_APP_HEAD}
                     <br />Name of the head branch received from a Git provider.
                 </li>
                 <li>
-                    COMMIT_REF: {process.env.COMMIT_REF}
+                    COMMIT_REF: {process.env.REACT_APP_COMMIT_REF}
                     <br />Reference of the commit we’re building.
                 </li>
                 <li>
-                    CONTEXT: {process.env.CONTEXT}
+                    CONTEXT: {process.env.REACT_APP_CONTEXT}
                     <br />Name of the context a deploy is built around, it can be production, deploy-preview or
                     branch-deploy.
                 </li>
                 <li>
-                    REVIEW_ID: {process.env.REVIEW_ID}
+                    REVIEW_ID: {process.env.REACT_APP_REVIEW_ID}
                     <br />the ID of the pull request and deploy preview (e.g. 1211). These two numbers will always match
                     (deploy-preview-12 is for PR # 12 in your repository)
                 </li>
