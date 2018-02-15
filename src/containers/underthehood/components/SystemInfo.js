4f590210842f286da11bc2e9bac22d549d486355
Peter Petrovics <szerintedmi@gmail.com>
2018-02-15 03:20:03 +0000

    env info tweaks on underthehood

--- src/containers/underthehood/components/SystemInfo.js
+++ src/containers/underthehood/components/SystemInfo.js
@@ -5,44 +5,27 @@ export function SystemInfo(props) {
     return (
         <Pblock header="System info">
             <p>NODE_ENV: {process.env.NODE_ENV} </p>
-
             {/*netlify env variables, see: https://www.netlify.com/docs/continuous-deployment/#build-environment-variables */}
             <p>Netlify env variables:</p>
-            <ul>
-                <li>
-                    REPOSITORY_URL: {process.env.REACT_APP_REPOSITORY_URL}
-                    <br />URL to the Git repository the build pulls changes from.
-                </li>
-                <li>
-                    BRANCH: {process.env.REACT_APP_BRANCH}
-                    <br />Reference to check out after fetching changes from the Git repository.
-                </li>
-                <li>
-                    PULL_REQUEST: {process.env.REACT_APP_PULL_REQUEST}
-                    <br />Whether the build is from a pull request or not.
-                </li>
-                <li>
-                    HEAD: {process.env.REACT_APP_HEAD}
-                    <br />Name of the head branch received from a Git provider.
-                </li>
-                <li>
-                    COMMIT_REF: {process.env.REACT_APP_COMMIT_REF}
-                    <br />Reference of the commit we’re building.
-                </li>
-                <li>
-                    CONTEXT: {process.env.REACT_APP_CONTEXT}
-                    <br />Name of the context a deploy is built around, it can be production, deploy-preview or
-                    branch-deploy.
-                </li>
-                <li>
-                    REVIEW_ID: {process.env.REACT_APP_REVIEW_ID}
-                    <br />the ID of the pull request and deploy preview (e.g. 1211). These two numbers will always match
-                    (deploy-preview-12 is for PR # 12 in your repository)
-                </li>
-                <li>URL: {process.env.REACT_APP_URL}</li>
-                <li>DEPLOY_URL: {process.env.REACT_APP_DEPLOY_URL}</li>
-                <li>DEPLOY_PRIME_URL: {process.env.REACT_APP_DEPLOY_PRIME_URL}</li>
-            </ul>
+            REPOSITORY_URL: {process.env.REACT_APP_REPOSITORY_URL}
+            <br />
+            BRANCH: {process.env.REACT_APP_BRANCH}
+            <br />
+            PULL_REQUEST: {process.env.REACT_APP_PULL_REQUEST}
+            <br />
+            HEAD: {process.env.REACT_APP_HEAD}
+            <br />
+            COMMIT_REF: {process.env.REACT_APP_COMMIT_REF}
+            <br />
+            CONTEXT: {process.env.REACT_APP_CONTEXT}
+            <br />
+            REVIEW_ID: {process.env.REACT_APP_REVIEW_ID}
+            <br />
+            URL: {process.env.REACT_APP_URL}
+            <br />
+            DEPLOY_URL: {process.env.REACT_APP_DEPLOY_URL}
+            <br />
+            DEPLOY_PRIME_URL: {process.env.REACT_APP_DEPLOY_PRIME_URL}
         </Pblock>
     );
 }
