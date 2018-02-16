f410ad3d72df4206a414995bc65eb6ea1c58ee5f
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-02-16 16:05:52 +0100

    [roadmap] use global media queries

--- src/containers/roadmap/style.js
+++ src/containers/roadmap/style.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
+import { media, mediaopacity } from '../../styles/media';
 
 export const StyleRoadmapLine = styled.img`
     padding: 0px 135px;
     max-width: 100%;
+    ${media.tablet`display: none;`}
 `;
 
 export const StyleRoadmapState = styled.div`
     width:270px;
+    ${media.tablet`
+      margin: auto;
+      margin-bottom: 65px;
+    `}
 `;
 
 export const StyleRoadmap = styled.div`
@@ -14,6 +20,10 @@ export const StyleRoadmap = styled.div`
     margin-top: 75px;
     padding: 0 20px;
     max-width: 1167px;
+
+    &>div {
+      ${media.tablet`margin: auto;`}
+    }
 
     &>div:nth-child(odd) ${StyleRoadmapLine} {
       -moz-transform: scaleX(-1);
@@ -26,6 +36,27 @@ export const StyleRoadmap = styled.div`
 
     &>div:nth-child(odd) ${StyleRoadmapState} {
       float: right;
+      ${media.tablet`float: none;`}
     }
 
+    & h4:not(.state),
+    h5,
+    p {
+      margin-top: 0;
+      ${mediaopacity.handheld`opacity: .6`}
+    }
+
+    & h4 {
+      margin: 0;
+    }
+
+    & h4.state {
+      margin-bottom: 14px;
+    }
+
+    & p {
+      font-size: 15px;
+      margin: 0;
+      margin-bottom: 5px;
+    }
 `;
