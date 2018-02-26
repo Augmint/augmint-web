cbc58b3f2971416212e8907cf08eaaaba194e0fe
Peter Petrovics <szerintedmi@gmail.com>
2018-02-26 17:44:16 +0000

    further gas estimate adjustments

--- src/modules/ethereum/gas.js
+++ src/modules/ethereum/gas.js
@@ -1,7 +1,7 @@
 export const cost = {
     NEW_LOAN_GAS: 200000, // As of now it's on ganache: 167,196-167390 - 182,000???
     NEW_FIRST_LOAN_GAS: 240000, // 227390
-    REPAY_GAS: 120000, // AugmintToken.transferAndNotify, as of now on testRpc: first: 105,354
+    REPAY_GAS: 150000, // AugmintToken.transferAndNotify, as of now on testRpc: first: 105,354, tehn : 120305 ?
     COLLECT_BASE_GAS: 90000, // as of now on testRpc: 1 loan = first: 73,333, consecutive:  64,683
     COLLECT_ONE_GAS: 20000, // as of now: ca. 10000
 
@@ -9,7 +9,7 @@ export const cost = {
     //consecutive : no narr: 45405 - 60405 (higher when sent to account which never received)
     // w narrative: 46733 - 56693
 
-    PLACE_ORDER_GAS: 250000,
-    MATCH_ORDERS_GAS: 200000,
-    CANCEL_ORDER_GAS: 150000
+    PLACE_ORDER_GAS: 200000,
+    MATCH_ORDERS_GAS: 100000,
+    CANCEL_ORDER_GAS: 60000
 };
