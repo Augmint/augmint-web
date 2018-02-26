8508581585352c236e841d3085b42a97a93a8faf
Peter Petrovics <szerintedmi@gmail.com>
2018-02-26 14:26:01 +0000

    update gas estimates

--- src/modules/ethereum/gas.js
+++ src/modules/ethereum/gas.js
@@ -1,9 +1,9 @@
 export const cost = {
-    NEW_LOAN_GAS: 400000, // As of now it's on testRPC: first for account = 296983  additional =  287467 first for user: testrpc: 320528
-    NEW_FIRST_LOAN_GAS: 400000, // on test rpc: 347467 rinkeby: ??
-    REPAY_GAS: 200000, // AugmintToken.repayLoan, as of now on testRpc: 108373 - 108461, 2nd from same AC: ?. on rinkeby: ???
-    COLLECT_BASE_GAS: 90000, // as of now on testRpc: 1 loan = first: 72639, 2nd: 63989
-    COLLECT_ONE_GAS: 40000, // as of now: ???
+    NEW_LOAN_GAS: 210000, // As of now it's on ganache: first = 197,196  consecutive: 167,196
+    NEW_FIRST_LOAN_GAS: 180000,
+    REPAY_GAS: 120000, // AugmintToken.transferAndNotify, as of now on testRpc: first: 105,354
+    COLLECT_BASE_GAS: 90000, // as of now on testRpc: 1 loan = first: 73,333, consecutive:  64,683
+    COLLECT_ONE_GAS: 20000, // as of now: ca. 10000
 
     TRANSFER_AUGMINT_TOKEN_GAS: 100000, // on testrpc: first: 75189 - 75405, rinkeby first: 76629
     //consecutive : no narr: 45405 - 60405 (higher when sent to account which never received)
