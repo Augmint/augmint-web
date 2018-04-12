2860bcdfd267d516e6d03c5e43016990119750c8
Peter Petrovics <szerintedmi@gmail.com>
2018-04-12 12:40:57 +0100

    fetch user locks to store + handle newlock event

--- src/modules/lockManagerProvider.js
+++ src/modules/lockManagerProvider.js
@@ -1,9 +1,10 @@
 import store from "modules/store";
 import { setupWatch } from "./web3Provider";
 import { connectLockManager, refreshLockManager, fetchLockProducts } from "modules/reducers/lockManager";
-// import { fetchLoansForAddress } from "modules/reducers/loans";
-// import { refreshAugmintToken } from "modules/reducers/augmintToken";
-// import { fetchUserBalance } from "modules/reducers/userBalances";
+import { fetchLoanProducts } from "modules/reducers/loanManager";
+import { refreshMonetarySupervisor } from "modules/reducers/monetarySupervisor";
+import { fetchLocksForAddress, processNewLock } from "modules/reducers/locks";
+import { fetchUserBalance } from "modules/reducers/userBalances";
 
 export default () => {
     const lockManager = store.getState().lockManager;
@@ -25,21 +26,22 @@ export default () => {
 };
 
 const setupListeners = () => {
-    //const lockManager = store.getState().lockManager.contract.ethersInstance;
-    // lockManager.onnewloan = onNewLoan;
-    // lockManager.onloanrepayed = onLoanRepayed;
-    // lockManager.onloancollected = onLoanCollected;
+    const lockManager = store.getState().lockManager.contract.ethersInstance;
+    lockManager.onnewlockproduct = onNewLockProduct;
+    lockManager.onlockproductactivechange = onLockProductActiveChange;
+    lockManager.onnewlock = onNewLock;
+    // TODO: LockReleased event
 };
 
 const refresLockManagerIfNeeded = (newVal, oldVal) => {
     if (newVal && store.getState().augmintToken.isConnected) {
         console.debug(
-            "lockManagerProvider - new augmintToken or loanManager contract. Dispatching refreshLockManager, fetchProducts, fetchLocks"
+            "lockManagerProvider - new augmintToken or loanManager contract. Dispatching refreshLockManager, fetchProducts, fetchLocksForAddress"
         );
-        // const userAccount = store.getState().web3Connect.userAccount;
+        const userAccount = store.getState().web3Connect.userAccount;
         store.dispatch(refreshLockManager());
         store.dispatch(fetchLockProducts());
-        // store.dispatch(fetchLoansForAddress(userAccount));
+        store.dispatch(fetchLocksForAddress(userAccount));
         setupListeners();
     }
 };
@@ -54,4 +56,50 @@ const onWeb3NetworkChange = newVal => {
 
 const onContractChange = (newVal, oldVal) => {
     refresLockManagerIfNeeded(newVal, oldVal);
+};
+
+const onNewLockProduct = (lockProductId, perTermInterest, durationInSecs, minimumLockAmount, isActive) => {
+    // event NewLockProduct(uint32 indexed lockProductId, uint32 perTermInterest, uint32 durationInSecs,
+    //                         uint32 minimumLockAmount, bool isActive);
+    console.debug("lockManagerProvider.onNewLockProduct: dispatching refreshLockManager and fetchLockProducts");
+    store.dispatch(refreshLockManager()); // to update product count
+    store.dispatch(fetchLockProducts()); // to fetch new product
+};
+
+const onLockProductActiveChange = (lockProductId, newActiveState) => {
+    // event LockProductActiveChange(uint32 indexed lockProductId, bool newActiveState);
+    console.debug("lockManagerProvider.onLockProductActiveChange: dispatching fetchLockProducts");
+    store.dispatch(fetchLockProducts()); // to refresh product list
+};
+
+const onNewLock = (lockOwner, lockId, amountLocked, interestEarned, lockedUntil, perTermInterest, durationInSecs) => {
+    // event NewLock(address indexed lockOwner, uint lockId, uint amountLocked, uint interestEarned,
+    //                 uint40 lockedUntil, uint32 perTermInterest, uint32 durationInSecs);
+    console.debug(
+        "lockManagerProvider.onNewLock: dispatching refreshLockManager, fetchLockProducts, fetchLoanProducts & refreshMonetarySupervisor"
+    );
+    store.dispatch(refreshMonetarySupervisor()); // to update totalLockAmount
+    store.dispatch(refreshLockManager()); // to update lockCount
+    store.dispatch(fetchLockProducts()); // to update maxLockAmounts
+    if (store.getState().loanManager.isConnected) {
+        store.dispatch(fetchLoanProducts()); // to update maxLoanAmounts
+    }
+    const userAccount = store.getState().web3Connect.userAccount;
+    if (lockOwner.toLowerCase() === userAccount.toLowerCase()) {
+        console.debug(
+            "lockManagerProvider.onNewLock: new lock for current user. Dispatching processNewLock & fetchUserBalance"
+        );
+        store.dispatch(
+            processNewLock(userAccount, {
+                lockOwner,
+                lockId,
+                amountLocked,
+                interestEarned,
+                lockedUntil,
+                perTermInterest,
+                durationInSecs
+            })
+        );
+        store.dispatch(fetchUserBalance(userAccount));
+    }
 };
