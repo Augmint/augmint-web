f9a142e5e8265acfdd20c131c0c51e946b50f087
Laszlo Heves <heves.laszlo@gmail.com>
2018-04-23 14:22:10 +0100

    add feature service

---
+++ src/modules/services/featureService/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const FEATURE_PREFIX = 'f_';
+
+// set deterministic flags
+const systemFeatures = {};
+
+function addFeatureFlag(param) {
+    // validate
+    // turn to boolean ?
+    const flag = param.split('=');
+    const flagKey = flag[0].substr(2);
+    const flagVal = flag[1] === 'false' ? false : flag[1] === 'true' ? true : flag[1];
+
+    return {
+        [flagKey]: flagVal
+    };
+}
+
+export function getFeaturesFromUrl() {
+    const urlParams = window.location.search.substr(1);
+    let userFeatures = {};
+
+    if (urlParams.includes(FEATURE_PREFIX)) {
+        if (urlParams.includes('&')) {
+            userFeatures = urlParams.split('&')
+                .filter(param => param.includes(FEATURE_PREFIX))
+                .map(param => addFeatureFlag(param))
+                .reduce(function(acc, val) {
+                    const currKey = Object.keys(val)[0];
+                    acc[currKey] = val[currKey];
+                    return acc;
+                }, {});
+        } else {
+            userFeatures = addFeatureFlag(urlParams);
+        }
+    }
+    return userFeatures;
+}
+
+function getCombinedFeatures() {
+    return Object.assign({}, systemFeatures, getFeaturesFromUrl());
+}
+
+export const FeatureContext = React.createContext(getCombinedFeatures());
