16bd5808e54842fea1399e13a8f149b30e80528b
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-04-26 17:34:01 +0200

    [header] review fix

--- src/components/augmint-ui/header/styles.js
+++ src/components/augmint-ui/header/styles.js
@@ -3,6 +3,15 @@ import theme from "styles/theme";
 
 const BaseHeader = `
     color: ${theme.colors.white};
+    font-family: MaisonNeue,'Helvetica Neue',Arial,Helvetica,sans-serif;
+    font-weight: 200;
+    padding: 0;
+`;
+
+const HeaderH3 = `
+    font-size: 26px;
+    line-height: 26px;
+    margin-bottom: 14px;
 `;
 
 export const StyledHeaderH3 = styled.h3`
