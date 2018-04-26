992e84f721acd9061b84ddbc3cf4a9507122e770
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-04-26 19:00:39 +0200

    [theme] add typography [header] use new theme variables

--- src/components/augmint-ui/header/styles.js
+++ src/components/augmint-ui/header/styles.js
@@ -3,15 +3,15 @@ import theme from "styles/theme";
 
 const BaseHeader = `
     color: ${theme.colors.white};
-    font-family: MaisonNeue,'Helvetica Neue',Arial,Helvetica,sans-serif;
+    font-family: ${theme.typography.fontFamilies.default};
     font-weight: 200;
     padding: 0;
 `;
 
 const HeaderH3 = `
-    font-size: 26px;
-    line-height: 26px;
-    margin-bottom: 14px;
+    font-size: ${theme.typography.fontSizes.h3};
+    line-height: ${theme.typography.fontSizes.h3};
+    margin-bottom: 0,875rem;
 `;
 
 export const StyledHeaderH3 = styled.h3`
