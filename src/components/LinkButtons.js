13a920306ec16ef37b097597d71b96e85c10c97f
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-05-02 14:45:11 +0200

    [linkButton] use augmint-ui button component

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { Segment, Image } from "semantic-ui-react";
+import Button from "./augmint-ui/button";
 
 import discordSVG from "assets/images/Discord-Logo.svg";
 
 export function DiscordButton(props) {
     return (
-        <Segment
-            basic
-            className="discord"
-            size="small"
-            as="a"
-            href="https://discord.gg/PwDmsnu"
-            target="_blank"
-            style={{ color: "#ffad00", display: "block", fontSize: 18, lineHeight: 1.29, margin: 0, padding: 0 }}
-        >
-            <Image src={discordSVG} style={{ display: "inline-block", marginRight: 20, maxWidth: 40 }}/>
+        <Button basic="true" className="discord" href="https://discord.gg/PwDmsnu" target="_blank">
+            <img src={discordSVG} style={{ display: "inline-block", marginRight: 20, maxWidth: 40 }} />
             Ask for help on our Discord
-        </Segment>
+        </Button>
     );
 }
