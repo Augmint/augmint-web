2802b4857ffd84a42de8de0a0b70c2c0d68c03a9
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-05-02 16:08:45 +0200

    [home] icon position fix

--- src/containers/home/NotConnectedHome/styles.js
+++ src/containers/home/NotConnectedHome/styles.js
@@ -1,62 +1,63 @@
 export const keyFeaturesSegment = {
-    minHeight: '125px',
-    display: 'flex',
-    alignItems: 'center',
+    minHeight: "125px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
 };
 
 export const keyFeaturesHeader = {
-    marginBottom: '14px',
-    textTransform: 'uppercase',
+    marginBottom: "14px",
+    textTransform: "uppercase"
 };
 
 export const howToUseHeader = {
-    marginBottom: '14px',
-    marginTop: '5px',
-    textTransform: 'uppercase',
-    textAlign: 'center',
+    marginBottom: "14px",
+    marginTop: "5px",
+    textTransform: "uppercase",
+    textAlign: "center"
 };
 
 export const howToUseText = {
-    textAlign: 'justify',
+    textAlign: "justify"
 };
 
 export const useAEurButton = {
-    backgroundColor: 'white',
-    borderTopRightRadius: '14px',
-    borderBottomRightRadius: '14px',
-    color: '#01385a',
-    display: 'flex',
-    flexDirection: 'column',
-    padding: '25px 20px',
-    textAlign: 'center',
+    backgroundColor: "white",
+    borderTopRightRadius: "14px",
+    borderBottomRightRadius: "14px",
+    color: "#01385a",
+    display: "flex",
+    flexDirection: "column",
+    padding: "25px 20px",
+    textAlign: "center"
 };
 
 export const howItWorksRail = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
 };
 
 export const howItWorksRailBox = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#01385a',
-    fontSize: '35px',
-    lineHeight: '35px',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#01385a",
+    fontSize: "35px",
+    lineHeight: "35px"
 };
 
 export const howItWorksRailBoxSpan = {
-    fontSize: '14px',
-    lineHeight: '14px',
-    fontWeight: '300'
+    fontSize: "14px",
+    lineHeight: "14px",
+    fontWeight: "300"
 };
 
 export const howItWorksImage = {
-    minHeight: '79px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    minHeight: "79px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
 };
