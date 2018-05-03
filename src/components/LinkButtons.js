9914636a8f0d8d25c9d96c2747abb19a5a1c6b4e
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-05-03 12:00:06 +0200

    [linkButton] add alt tag

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -6,7 +6,11 @@ import discordSVG from "assets/images/Discord-Logo.svg";
 export function DiscordButton(props) {
     return (
         <Button className="discord" href="https://discord.gg/PwDmsnu" target="_blank">
-            <img src={discordSVG} style={{ display: "inline-block", marginRight: 20, maxWidth: 40 }} />
+            <img
+                alt="discord icon"
+                src={discordSVG}
+                style={{ display: "inline-block", marginRight: 20, maxWidth: 40 }}
+            />
             Ask for help on our Discord
         </Button>
     );
