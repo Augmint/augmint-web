281cad23ebda32f9d2dc95835684eda21c788b0f
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-05-07 16:13:31 +0200

    [divider] add new augment-ui component

---
+++ src/components/augmint-ui/divider/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import theme from "styles/theme";
+
+const BaseDiv = `
+border-top: 1px solid rgba(34,36,38,.15);
+border-bottom: 1px solid rgba(255,255,255,.1);
+margin: 1rem 0;
+height: 0;
+user-select: none;
+`;
+
+export const StyledDiv = styled.div`
+    ${BaseDiv};
+`;
