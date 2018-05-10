7c7bb918063ab83970a19375e0f0cbd0b8086975
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-05-10 16:45:21 +0200

    [list] create new augment-ui component

---
+++ src/components/augmint-ui/list/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import theme from "styles/theme";
+import { remCalc } from "styles/theme";
+import { media } from "styles/media";
+
+const BaseStyle = `
+    color: ${theme.colors.opacWhite};
+    display: flex;
+    font-family: ${theme.typography.fontFamilies.default};
+    font-size: ${remCalc(14)};
+    justify-content: center;
+    letter-spacing: 2.4px;
+    margin: 0 ${remCalc(20)};
+    text-align: center;
+    text-transform: uppercase;
+
+    &:hover {
+      color: ${theme.colors.white};
+    }
+
+    & a {
+      color: inherit;
+    }
+`;
+
+export const StyledItem = styled.div`
+    ${BaseStyle};
+`;
+
+export const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+`;
