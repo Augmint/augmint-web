3c75094f20bc0175197018df1b85c803d2d38059
Adam Ridovics <adam.ridovics@gmail.com>
2018-06-07 17:56:22 +0200

    fix(typo): delete unnecessary commented line

--- src/components/MyListGroups.js
+++ src/components/MyListGroups.js
@@ -27,7 +27,6 @@ export function MyListGroupColumn(props) {
     const { children, header, style = { padding: "0.5em 0", margin: "0em 0" }, width, ...other } = props;
     return (
         <Grid.Unit style={style} size={width} {...other}>
-            {/* {header && <Header as="h3">{header}</Header>} */}
             {header && 
                 <FeatureContext>
                     {features => features.dashboard 
