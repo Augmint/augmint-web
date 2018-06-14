2af8859c70169777b12e33b2701d7e4825eda498
Adam Ridovics <adam.ridovics@gmail.com>
2018-06-14 16:11:13 +0200

    fix(underthehood, menu): add color

--- src/components/augmint-ui/menu/styles.js
+++ src/components/augmint-ui/menu/styles.js
@@ -12,6 +12,10 @@ export const StyledMenu = styled.div`
     line-height: ${remCalc(20)};
     margin-bottom: ${remCalc(14)};
     min-height: ${remCalc(50)};
+
+    &.dashboardColor {
+      border-bottom: 1px solid ${theme.colors.primary};
+    }
 `;
 
 export const StyledMenuItem = styled.a`
@@ -29,6 +33,26 @@ export const StyledMenuItem = styled.a`
     &.active {
       background-color: ${theme.colors.white};
       border-radius: ${theme.borderRadius.top};
+    }
+}
+`;
+
+export const StyledMenuItemDashboard = styled.a`
+    align-items: center;
+    color: ${theme.colors.primary};
+    cursor: pointer;
+    line-height: ${remCalc(18)};
+    padding: ${remCalc(16)} ${remCalc(25)};
+
+    &.active,
+    &:hover {
+      color: ${theme.colors.secondary};
+    }
+
+    &.active {
+      background-color: ${theme.colors.primary};
+      border-radius: ${theme.borderRadius.top};
+      color: ${theme.colors.white};
     }
 }
 `;
