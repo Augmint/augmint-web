2b2b5e62691865764a74910db4684ec7b24f14ca
Gyorgy Csado <gyorgy.csado@gmail.com>
2018-06-20 15:23:29 +0200

    [converter] shortAccountAddress length fix

--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,10 +1,9 @@
 export function shortAccountAddresConverter(address) {
-    return address.substring(0, 3) + "..." + address.substring(address.length - 4, address.length);
+    return address.substring(0, 6) + "..." + address.substring(address.length - 4, address.length);
 }
 
 export function floatNumberConverter(floatedNumber, decimals) {
     const numberArray = String(floatedNumber).split(".");
     numberArray[1] = numberArray[1] || "00000000000000000000000000000000000";
     return Number(numberArray[0] + numberArray[1].slice(0, decimals) + "." + numberArray[1].slice(decimals));
-
 }
