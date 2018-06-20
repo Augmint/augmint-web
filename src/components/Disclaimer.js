1cfa9d37e2b18572b8e2146eb169a2917aed041b
boros.gergely <boros.gergely@virgo.hu>
2018-06-20 13:02:33 +0200

    feat(disclaimer): add disclaimer modal

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { default as Modal, ModalActions, ModalContent, ModalHeader } from "./augmint-ui/modal";
+import Button from "./augmint-ui/button";
+
+const dismissedCookie = 'disclaimerDismissed=true';
+
+export default class Disclaimer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const dismissed = document.cookie
+          .split(";")
+          .map(cookie => cookie.trim())
+          .includes(dismissedCookie);
+
+        this.state = { dismissed };
+        this.close = this.close.bind(this);
+    }
+
+    close() {
+        document.cookie = dismissedCookie;
+        this.setState({ dismissed: true });
+    }
+
+    render() {
+        return (
+            !this.state.dismissed && (
+                <Modal showClose={true} onCloseRequest={this.close}>
+                    <ModalContent>
+                        <ModalHeader>
+                            <h3 style={{marginTop: 0}}>
+                                Augmint beta test
+                            </h3>
+                        </ModalHeader>
+                        Augmint is in beta test. The tokens issued by Augmint contracts are not legal tender. Use them at your own risk. Do not participate unless you have read their <Link to="/disclaimer" onClick={this.close}>disclaimer</Link>.
+                    </ModalContent>
+                    <ModalActions>
+                        <Button onClick={this.close}>
+                            Got it
+                        </Button>
+                    </ModalActions>
+                </Modal>
+            )
+        );
+    }
+}
