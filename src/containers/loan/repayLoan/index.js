407b9b8ed8af9bab40025526fe8157595a659326
Adam Ridovics <adam.ridovics@gmail.com>
2018-06-25 15:53:23 +0200

    refactor(tryIt, TryItConnected, loanDetailsPage, newLoan, repayLoan): remove dashboard feature flag

--- src/containers/loan/repayLoan/index.js
+++ src/containers/loan/repayLoan/index.js
@@ -4,20 +4,12 @@ import RepayLoanPage from "./RepayLoanPage";
 import LoanSelector from "./LoanSelector";
 import { Psegment, Pheader } from "components/PageLayout";
 import TopNavTitlePortal from 'components/portals/TopNavTitlePortal';
-import { FeatureContext } from "modules/services/featureService";
 
 const repayLoanMain = () => (
     <Psegment>
-        <FeatureContext>
-            {features => {
-                const dashboard = features.dashboard;
-                return (
-                    <TopNavTitlePortal>
-                        <Pheader className={ dashboard ? "secondaryColor" : "" } header="Repay loan" />
-                    </TopNavTitlePortal>
-                );
-            }}
-        </FeatureContext>
+        <TopNavTitlePortal>
+            <Pheader className={ "secondaryColor" } header="Repay loan" />
+        </TopNavTitlePortal>
 
         <Route exact path="/loan/repay" component={LoanSelector} />
         <Route path="/loan/repay/:loanId" component={RepayLoanPage} />
