7b42dc6646e5454a0999832d10dc5c707fc7831f
Adam Ridovics <adam.ridovics@gmail.com>
2018-06-25 13:50:15 +0200

    refactor(button): remove dashboard feature flag and keep the new design

--- src/components/augmint-ui/button/index.js
+++ src/components/augmint-ui/button/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import Icon from "../icon";
 
 import { StyledLink, StyledA, StyledButton } from "./styles";
+// import { FeatureContext } from "modules/services/featureService";
 
 export default function Button(props) {
-    const { children, to, type, content, icon } = props;
+    const { children, to, type, content, icon, className } = props;
 
+    let _className = className + " dashboardColors";
     let elementType = StyledA,
         _icon;
 
@@ -17,6 +19,7 @@ export default function Button(props) {
     if (icon) {
         _icon = <Icon name={icon} />;
     }
-    
-    return React.createElement(elementType, props, children, content, _icon)
+
+
+    return React.createElement(elementType, {...props, className: _className }, children, content, _icon)
 }
