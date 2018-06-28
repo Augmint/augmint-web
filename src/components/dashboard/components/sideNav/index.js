49c49b9b40f7afe891bf16efd60cb1c93689ca5f
Adam Ridovics <adam.ridovics@gmail.com>
2018-06-28 16:55:17 +0200

    fix(sideNav, navigatoin, app/index & site): add responsive sideNav & change AppMenu to SiteMenu

--- src/components/dashboard/components/sideNav/index.js
+++ src/components/dashboard/components/sideNav/index.js
@@ -6,8 +6,31 @@ import Icon from "components/augmint-ui/icon";
 
 import styled from "styled-components";
 import theme from "styles/theme";
+import { media, mediaopacity } from "styles/media";
 
 import AugmintLogo from "assets/images/logo/augmint.svg";
+import hamburgerMenu from "assets/images/menu.svg";
+import close from "assets/images/close.svg";
+
+export const HamburgerMenu = styled.img`
+    display: none;
+    height: 32px;
+    width: 32px;
+    margin-top: 15px;
+    visibility: hidden;
+
+    ${media.tablet`
+      display: block;
+      visibility: visible;
+    `};
+
+    &.opened {
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin-left: 15px;
+    }
+`;
 
 export const SideNav = styled.nav`
     position: fixed;
@@ -21,10 +44,33 @@ export const SideNav = styled.nav`
     height: 100%;
     z-index: 104;
     padding: 20px 0;
+    
+    &.closed {
+        ${media.tablet`
+            width: 60px;
+            height: 60px;
+            padding: 0;
+        `};
+    }
 
-    img {
+    &.opened {
+        ${mediaopacity.handheld`opacity: .95`} width: 100%;
+    }
+
+    img:not(${HamburgerMenu}) {
         width: 60px;
         margin-bottom: 20px;
+        display: block;
+        visibility: visible;
+
+        ${media.tablet`
+            margin-top: 40px; 
+            margin-bottom: 0;
+            &.hidden{   
+                display: none;
+                visibility: hidden;    
+            }
+        `};
     }
 `;
 
@@ -35,11 +81,25 @@ export const SideNavUl = styled.ul`
     width: 100%;
     list-style: none;
     padding: 0;
+
+    ${media.tablet`
+      display: block;
+      visibility: visible;
+      &.hidden {
+        display: none;
+        visibility: hidden;
+        }
+    `};
 `;
 
 export const SideNavLi = styled.li`
     margin-bottom: 10px;
     width: 100%;
+    ${media.tablet`
+        max-width: 150px;
+        margin: auto;
+        text-align: left;
+    `}
 `;
 
 export const SideNavLink = styled(NavLink)`
@@ -62,6 +122,9 @@ export const SideNavLink = styled(NavLink)`
         color: ${theme.colors.secondary};
         display: flex;
         align-items: center;
+        ${media.tablet`
+            border: none;
+        `}
     }
 
     > i {
@@ -72,49 +135,75 @@ export const SideNavLink = styled(NavLink)`
     }
 
     > span {
-        margin-left: 12px;
+        margin-left: 15px;
         line-height: 1.5rem;
         text-transform: uppercase;
         font-size: 12px;
+        ${media.tablet`
+            font-size: 20px;
+            line-height: 2.4rem;
+        `}
     }
 `;
 
-export default props => (
-    <SideNav>
-        <NavLink to="/">
-            <img alt="Augmint" src={AugmintLogo} />
-        </NavLink>
-        <SideNavUl>
-            <SideNavLi>
-                <SideNavLink to="/account" activeClassName="active" data-testid="myAccountMenuLink">
-                    <Icon name="account" />
-                    <span>Account</span>
-                </SideNavLink>
-            </SideNavLi>
-            <SideNavLi>
-                <SideNavLink to="/exchange" activeClassName="active" data-testid="exchangeMenuLink">
-                    <Icon name="exchange" />
-                    <span>Buy/Sell</span>
-                </SideNavLink>
-            </SideNavLi>
-            <SideNavLi>
-                <SideNavLink to="/loan/new" activeClassName="active" data-testid="getLoanMenuLink">
-                    <Icon name="loan" />
-                    <span>Loan</span>
-                </SideNavLink>
-            </SideNavLi>
-            <SideNavLi>
-                <SideNavLink to="/lock" activeClassName="active" data-testid="lockMenuLink">
-                    <Icon name="lock" />
-                    <span>Lock</span>
-                </SideNavLink>
-            </SideNavLi>
-            <SideNavLi>
-                <SideNavLink to="/reserves" activeClassName="active" data-testid="reservesMenuLink">
-                    <Icon name="reserves" />
-                    <span>Reserves</span>
-                </SideNavLink>
-            </SideNavLi>
-        </SideNavUl>
-    </SideNav>
-);
+
+// export default props => (
+export default class SiteNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+    toggleMenu() {
+        this.props.toggleMenu();
+    }
+
+    render() {
+
+        return(
+            // <SideNav className={this.props.showMenu ? "show" : "hidden"}>
+            <SideNav className={this.props.showMenu ? "opened" : "closed"}>
+                <HamburgerMenu
+                    src={this.props.showMenu ? close : hamburgerMenu}
+                    onClick={this.toggleMenu}
+                    id="hamburgerMenu"
+                    className={this.props.showMenu ? "opened" : ""}
+                />
+                <NavLink to="/">
+                    <img alt="Augmint" src={AugmintLogo} className={this.props.showMenu ? "" : "hidden"}/>
+                </NavLink>
+                <SideNavUl className={this.props.showMenu ? "" : "hidden"}>
+                    <SideNavLi>
+                        <SideNavLink to="/account" activeClassName="active" data-testid="myAccountMenuLink">
+                            <Icon name="account" />
+                            <span>Account</span>
+                        </SideNavLink>
+                    </SideNavLi>
+                    <SideNavLi>
+                        <SideNavLink to="/exchange" activeClassName="active" data-testid="exchangeMenuLink">
+                            <Icon name="exchange" />
+                            <span>Buy/Sell</span>
+                        </SideNavLink>
+                    </SideNavLi>
+                    <SideNavLi>
+                        <SideNavLink to="/loan/new" activeClassName="active" data-testid="getLoanMenuLink">
+                            <Icon name="loan" />
+                            <span>Loan</span>
+                        </SideNavLink>
+                    </SideNavLi>
+                    <SideNavLi>
+                        <SideNavLink to="/lock" activeClassName="active" data-testid="lockMenuLink">
+                            <Icon name="lock" />
+                            <span>Lock</span>
+                        </SideNavLink>
+                    </SideNavLi>
+                    <SideNavLi>
+                        <SideNavLink to="/reserves" activeClassName="active" data-testid="reservesMenuLink">
+                            <Icon name="reserves" />
+                            <span>Reserves</span>
+                        </SideNavLink>
+                    </SideNavLi>
+                </SideNavUl>
+            </SideNav>
+        )
+    }
+};
