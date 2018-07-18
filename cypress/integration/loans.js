f5d8fecc827ae3d8bb716eb47e02a8f6f29dd10d
Adam Ridovics <adam.ridovics@gmail.com>
2018-07-18 17:42:17 +0200

    fix(cypress/.../loans): add scrollIntoView() to all the msgPanels that is not visible

--- cypress/integration/loans.js
+++ cypress/integration/loans.js
@@ -46,7 +46,9 @@ describe("Loans", function() {
             cy.get("[data-testid=EthSubmissionSuccessPanel] >[data-testid=msgPanelOkButton]").click();
 
             cy.get("[data-testid=EthConfirmationReceivedPanel]").should("contain", "confirmation");
-            cy.get("[data-testid=EthConfirmationReceivedPanel] > [data-testid=msgPanelOkButton]").click();
+            cy.get("[data-testid=EthConfirmationReceivedPanel] > [data-testid=msgPanelOkButton]")
+                .scrollIntoView()
+                .click();
 
             cy.get("[data-testid=loansToCollectBlock]", { timeout: 8000 }) // increase timeout b/c of occassional Travis timeouts
                 .should("contain", "No defaulted and uncollected loan.");
