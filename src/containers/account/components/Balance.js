6e87c9a61f07b9b2dee57164a2337f4e7d964452
boros.gergely <boros.gergely@virgo.hu>
2018-08-06 13:12:23 +0200

    feat(account): add balance card to account page

---
+++ src/containers/account/components/Balance.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import { default as theme, remCalc } from "styles/theme";
+import { Pgrid, Pblock } from "components/PageLayout";
+
+const Label = styled.div`
+    font-size: ${remCalc(14)};
+    color: ${theme.colors.mediumGrey};
+    &.balance {
+        text-align: left;
+    }
+`;
+
+const TokenBalance = styled.div`
+    font-family: ${theme.typography.fontFamilies.currency};
+    font-size: ${remCalc(48)};
+    .currency {
+        font-size: ${remCalc(24)};
+    }
+`;
+
+const TokenAmount = styled.div`
+    font-family: ${theme.typography.fontFamilies.currency};
+    font-size: ${remCalc(24)};
+`;
+
+const EthAmount = styled.div`
+    margin-top: 0.5em;
+    padding: 0.5em 0;
+    border-top: 1px solid #ccc;
+    font-family: ${theme.typography.fontFamilies.currency};
+`;
+
+export default class Balance extends React.Component {
+    render() {
+        const { userAccount, loans, locks } = this.props;
+        const loansAmount = loans.loans && loans.loans.reduce((acc, loan) => acc + loan.loanAmount, 0);
+        const locksAmount = locks.locks && locks.locks.reduce((acc, lock) => acc + lock.amountLocked, 0);
+        return (
+            <Pblock style={{ textAlign: "center" }}>
+                <div>
+                    <Label className="balance">Current balance</Label>
+                    <TokenBalance>
+                        {userAccount.tokenBalance}
+                        <span className="currency"> A€</span>
+                    </TokenBalance>
+                </div>
+                <Pgrid.Row>
+                    <Pgrid.Column size={1 / 2}>
+                        <Label>My total loans</Label>
+                        <TokenAmount>
+                            {loansAmount} <span className="currency">A€</span>
+                        </TokenAmount>
+                    </Pgrid.Column>
+                    <Pgrid.Column size={1 / 2}>
+                        <Label>My total locks</Label>
+                        <TokenAmount>
+                            {locksAmount} <span className="currency">A€</span>
+                        </TokenAmount>
+                    </Pgrid.Column>
+                </Pgrid.Row>
+                <EthAmount>
+                    <Label>ETH balance</Label>
+                    <div>{userAccount.ethBalance} ETH</div>
+                </EthAmount>
+            </Pblock>
+        );
+    }
+}
