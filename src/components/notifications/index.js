90993fd6406393bd75dfc9446b71f2a9524d6aef
Adam Ridovics <adam.ridovics@gmail.com>
2018-08-17 15:13:22 +0200

    fix(notifications): change height to max-height & add overflow

--- src/components/notifications/index.js
+++ src/components/notifications/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import { connect } from "react-redux";
-// import { ConnectionStatus } from "components/MsgPanels";
 import store from "modules/store";
 import { dismissTx } from "modules/reducers/submittedTransactions";
 import { StyledNotificationPanel, StyledSpan, CloseIcon } from "./styles";
@@ -34,10 +32,6 @@ export class NotificationPanel extends React.Component {
                     }}
                     className={this.props.showNotificationPanel ? "opened" : ""}
                 />
-                {/* <g fill="none" fill-rule="evenodd" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                        <path d="M3.5 3.5l26.618 26.618M30.5 3.5L3.882 30.118"/>
-                    </g>
-                </StyledSvg> */}
                 {children}
             </StyledNotificationPanel>
         );
--- src/components/notifications/styles.js
+++ src/components/notifications/styles.js
@@ -11,7 +11,8 @@ export const StyledNotificationPanel = styled.div`
     width: 290px;
     /* max-width: 366px; */
     padding: 40px 10px 10px;
-    height: calc(100% - 120px);
+    max-height: calc(100% - 120px);
+    overflow: scroll;
 
     ${media.mobile`
         right: 0;
