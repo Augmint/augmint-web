e6608417a4e881aa1bb9a30bbb06d2f0f7bf752d
Treer Zoltán <treer@euedge.com>
2018-10-29 15:55:15 +0100

    remove legacy rinkeby addresses from frontend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -30,13 +30,7 @@ export const LEGACY_AEUR_CONTRACTS = {
     999: ["0x7f3abcd33ffe83ba7426ee6ec45014364fe1cab4"],
 
     // rinkeby
-    4: [
-        // "0x95aa79d7410eb60f49bfd570b445836d402bd7b1", // oldToken1 - DROPPED support on UI to convert to latest (monetarySupervisor would need to be NoFeeTransferContract on Token instead of latest feeAccount)
-        "0xA35D9de06895a3A2E7eCaE26654b88Fe71C179eA", // oldToken2 https://github.com/Augmint/augmint-web/commit/1f66ee910f5186c38733e1196ac5d41260490d24
-        "0x135893f1a6b3037bb45182841f18f69327366992", // oldToken3
-        "0x6C90c10D7A33815C2BaeeD66eE8b848F1D95268e", // oldToken4
-        "0xe54f61d6EaDF03b658b3354BbD80cF563fEca34c" // oldToken5
-    ]
+    4: []
 };
 
 /* List of old augmint token deploy addresses by network id */
@@ -48,13 +42,7 @@ export const LEGACY_LOANMANAGER_CONTRACTS = {
     999: ["0x5d77f09a3703be84d84810379067a6d9ad759582"],
 
     // rinkeby
-    4: [
-        // too old version, no CHUNK_SIZE(), didn't bother to make compatible: "0xfb505462633ae3234760d0ee51c557199ab249df",
-        "0xec5e35d8941386c3e08019b0ad1d4a8c40c7bcbc", // (oldToken2?)
-        "0xbdb02f82d7ad574f9f549895caf41e23a8981b07", // oldLoanManager1 (oldToken3)
-        "0x214919Abe3f2b7CA7a43a799C4FC7132bBf78e8A", // oldLoanManager2 (oldToken4)
-        "0x3b5DD323534659655EEccc642c3e338AAbD0B219" // oldLoanManager3 (oldToken5)
-    ]
+    4: []
 };
 
 /* List of old Locker deploy addresses by network id */
@@ -66,14 +54,7 @@ export const LEGACY_LOCKER_CONTRACTS = {
     999: ["0xe6a48098a0a318ccec66bcd8297417e0d74585dc"],
 
     // rinkeby
-    4: [
-        // too old version, no CHUNK_SIZE(), didn't bother to make compatible: "0x617cf9ba5c9cbecdd66412bc1d073b002aa26426",
-        "0xfb6b4803c590e564a3e6810289ab638b353a1367", // (oldToken2?)
-        "0xf98ae1fb568b267a7632bf54579a153c892e2ec2", // oldLocker1 (oldToken3)
-        "0xd0B6136C2E35c288A903E836feB9535954E4A9e9", // oldLocker2 (oldToken4)
-        "0x5B94AaF241E8039ed6d3608760AE9fA7186767d7", // oldLocker3 - tokencontract: 0xe54f61d6EaDF03b658b3354BbD80cF563fEca34c
-        "0xF74c0CB2713214808553CDA5C78f92219478863d" // oldLocker4 (oldToken5)
-    ]
+    4: []
 };
 
 /* List of old augmint token deploy addresses by network id */
@@ -88,13 +69,5 @@ export const LEGACY_EXCHANGE_CONTRACTS = {
     999: ["0x9f5420ec1348df8de8c85dab8d240ace122204c5"],
 
     // rinkeby
-    4: [
-        "0x65d30e5a6191a507fda96341f6ba773c4224c0e1",
-        "0x03fe291f8a30e54cd05459f368d554b40784ca78",
-        "0x86abc21cbb508fcb303f881d6871e4f870ce041a",
-        "0xc5b604f8e046dff26642ca544c9eb3064e02ecd9",
-        "0x5e2Be81aB4237c7c08d929c42b9F13cF4f9040D2",
-        "0x5c35162dbf91c794f1569c5fe1649f0c5283d2f6",
-        "0x554817688D096ae89fDacCf52E76f629B9Db8f53" // (oldToken5)
-    ]
+    4: []
 };
