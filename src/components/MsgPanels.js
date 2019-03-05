61500ab311e43500c380c6f74994c004895dfc7e
brigittaforrai <forraibrigi@gmail.com>
2019-03-05 15:06:25 +0100

    invalid onDismiss props fix

--- src/components/MsgPanels.js
+++ src/components/MsgPanels.js
@@ -65,7 +65,7 @@ export class MsgPanel extends React.Component {
             ...other
         } = this.props;
         let _className = className;
-        let noClose = false;
+        // let noClose = false;
 
         // always show notification close icon
         // if (loading) {
@@ -86,7 +86,7 @@ export class MsgPanel extends React.Component {
                     <Message
                         onDismiss={onDismiss ? this.dismiss : null}
                         className={_className}
-                        noCloseIcon={noClose}
+                        // noCloseIcon={noClose}
                         {...other}
                     >
                         {isNotification && (
