66d87db1b04f99b45dccd36637ab55f322987e29
szerintedmi <szerintedmi@gmail.com>
2019-03-27 18:52:54 +0000

    isOrderBetter test clarification

--- src/modules/ethereum/exchangeTransactions.test.js
+++ src/modules/ethereum/exchangeTransactions.test.js
@@ -1,31 +1,38 @@
 import { isOrderBetter, TOKEN_BUY, TOKEN_SELL } from "modules/ethereum/exchangeTransactions";
 
 describe("isOrderBetter", () => {
-    test("o1 should be better (price)", () => {
-        const o1 = { direction: TOKEN_SELL, price: 2, id: 2 };
-        const o2 = { direction: TOKEN_SELL, price: 1, id: 1 };
+    test("o2 should be better (SELL price)", () => {
+        const o1 = { direction: TOKEN_SELL, price: 2, id: 1 };
+        const o2 = { direction: TOKEN_SELL, price: 1, id: 2 };
         const result = isOrderBetter(o1, o2);
         expect(result).toBe(1);
     });
 
-    test("o1 should be better (id)", () => {
+    test("o1 should be better (BUY price)", () => {
+        const o1 = { direction: TOKEN_BUY, price: 2, id: 2 };
+        const o2 = { direction: TOKEN_BUY, price: 1, id: 1 };
+        const result = isOrderBetter(o1, o2);
+        expect(result).toBe(-1);
+    });
+
+    test("o2 should be better (SELL id)", () => {
         const o1 = { direction: TOKEN_SELL, price: 1, id: 2 };
         const o2 = { direction: TOKEN_SELL, price: 1, id: 1 };
         const result = isOrderBetter(o1, o2);
         expect(result).toBe(1);
     });
 
-    test("o2 should be better when o1 same as o2", () => {
+    test("o2 should be better (BUY id)", () => {
+        const o1 = { direction: TOKEN_BUY, price: 1, id: 2 };
+        const o2 = { direction: TOKEN_BUY, price: 1, id: 1 };
+        const result = isOrderBetter(o1, o2);
+        expect(result).toBe(1);
+    });
+
+    test("o1 should be better when o1 same as o2", () => {
         // same id for two orders, it shouldn't happen
         const o1 = { direction: TOKEN_SELL, price: 1, id: 1 };
         const o2 = { direction: TOKEN_SELL, price: 1, id: 1 };
-        const result = isOrderBetter(o1, o2);
-        expect(result).toBe(-1);
-    });
-
-    test("o1 should be better when o1 same as o2 but o1.id < o2.id", () => {
-        const o1 = { direction: TOKEN_SELL, price: 1, id: 1 };
-        const o2 = { direction: TOKEN_SELL, price: 1, id: 2 };
         const result = isOrderBetter(o1, o2);
         expect(result).toBe(-1);
     });
