7568b6ae7e0d1b2d29869472a74700f840b34b9c
juli_e <julia.ero@euedge.com>
2019-04-11 13:32:22 +0200

    edit mr coin copy

--- src/containers/funding/components/FundList/funds.js
+++ src/containers/funding/components/FundList/funds.js
@@ -5,8 +5,8 @@ export const FUNDS = [
         name: "MrCoin",
         buyUrl: "https://www.mrcoin.eu/buy/aeur?address=",
         sellUrl: "https://www.mrcoin.eu/sell/aeur?address=",
-        addFeatures: ["1:1 exchange rate", "no banking fees", "0.25% exchange fee"],
-        withdrawFeatures: ["banking fees apply"],
+        addFeatures: ["1:1 exchange rate", "0.25% exchange fee", "no banking fees"],
+        withdrawFeatures: ["1:1 exchange rate", "0.25% exchange fee", "banking fees apply"],
         image: mrCoinImg
     }
 ];
