d6777e51974d4ec69ffac73c16e9633e145cd2e9
Viktor Szathmáry <phraktle@gmail.com>
2019-04-16 23:33:15 +0200

    currencies: handle empty values

--- src/components/augmint-ui/currencies.js
+++ src/components/augmint-ui/currencies.js
@@ -8,6 +8,9 @@ const NoWrap = styled.span`
 `;
 
 function signum(n) {
+    if (n === undefined || n === null) {
+        return "empty";
+    }
     /*eslint eqeqeq: 0*/
     return n == 0 ? "zero" : n > 0 ? "positive" : "negative";
     /*eslint eqeqeq: 1*/
@@ -16,15 +19,16 @@ function signum(n) {
 /*
     amount: amount to display (an integer in the smallest unit of account)
     raw: set to true, if amount is a floating point num (the number will be printed without unit conversion)
+    decimals: default to token decimals
  */
 export class AEUR extends React.Component {
     render() {
-        const { amount, raw, className, ...rest } = this.props;
-        const amt = raw ? amount : amount / Math.pow(10, DECIMALS);
+        const { amount, raw, className, decimals = DECIMALS, ...rest } = this.props;
+        const amt = amount === undefined || raw ? amount : amount / Math.pow(10, DECIMALS);
         const cls = ["AEUR", className, signum(amt)].join(" ");
         return (
             <NoWrap className={cls} {...rest}>
-                {amt.toFixed(DECIMALS)} A€
+                {amt !== undefined && `${amt.toFixed(decimals)} A€`}
             </NoWrap>
         );
     }
@@ -38,11 +42,11 @@ export class AEUR extends React.Component {
 export class ETH extends React.Component {
     render() {
         const { amount, raw, className, decimals = 5, ...rest } = this.props;
-        const amt = raw ? amount : utils.fromWei(amount);
+        const amt = amount === undefined || raw ? amount : utils.fromWei(amount);
         const cls = ["ETH", className, signum(amt)].join(" ");
         return (
             <NoWrap className={cls} {...rest}>
-                {amt.toFixed(decimals)} ETH
+                {amt !== undefined && `${amt.toFixed(decimals)} ETH`}
             </NoWrap>
         );
     }
