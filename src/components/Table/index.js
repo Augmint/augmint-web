3af07343bab67e026956e35e6798a7dfd86fe618
juli_e <julia.ero@euedge.com>
2019-05-07 14:16:42 +0200

    add show more for transaction history

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 
 import { StyleTitle, StyleTable, StyleThead, StyleTbody, StyleTd, StyleTh, StyleTr } from "./style";
+import Button from "components/augmint-ui/button";
+
+const PAGING = 10;
 
 export class CustomTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initData: this.props.data,
+            dataToShow: this.props.data.slice(0, PAGING)
+        };
+        this.addData = this.addData.bind(this);
+    }
+
+    addData() {
+        const { dataToShow, initData } = this.state;
+        const lastIndex = initData.indexOf(dataToShow[dataToShow.length - 1]);
+
+        this.setState({
+            dataToShow: initData.slice(0, lastIndex + PAGING)
+        });
+    }
+
     render() {
-        const { datakeys, unit, data, headerdata, title, extraClass, testid, noItemsMessage } = this.props;
+        const { datakeys, data, unit, headerdata, title, extraClass, testid, noItemsMessage } = this.props;
+        const { dataToShow } = this.state;
 
         return (
             <div className={extraClass}>
@@ -24,7 +46,8 @@ export class CustomTable extends React.Component {
                     )}
                     <StyleTbody>
                         {data &&
-                            data.map((rowData, rowIndex) => (
+                            dataToShow &&
+                            dataToShow.map((rowData, rowIndex) => (
                                 <StyleTr key={"row_" + rowIndex}>
                                     {datakeys.map((key, index) => (
                                         <StyleTd key={"row_" + rowIndex + "_cell_" + index}>
@@ -36,6 +59,14 @@ export class CustomTable extends React.Component {
                             ))}
                     </StyleTbody>
                 </StyleTable>
+                <Button
+                    size="big"
+                    className="ghost"
+                    onClick={this.addData}
+                    style={{ marginTop: "1rem", marginLeft: "1rem" }}
+                >
+                    show older
+                </Button>
             </div>
         );
     }
