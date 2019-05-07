900a183ba8e9d68d36fe1e1b92d85c48ab9b6610
brigittaforrai <forraibrigi@gmail.com>
2019-05-07 11:31:09 +0200

    lock warning

--- src/containers/lock/containers/LockForm/index.js
+++ src/containers/lock/containers/LockForm/index.js
@@ -280,16 +280,11 @@ class LockContainer extends React.Component {
                             id="selectedLockProduct"
                             name="lockTerms"
                             data-testid="lock-product-selector"
-                            info={
-                                this.state.selectedProduct
-                                    ? `A-EUR unlock only available after ${this.state.selectedProduct.durationText}`
-                                    : ""
-                            }
                         />
 
                         {!!this.state.lockAmount && (
                             <div>
-                                <p style={{ marginTop: 0, marginBottom: 20, lineHeight: 1.5, textAlign: "center" }}>
+                                <p style={{ marginTop: 0, marginBottom: 10, lineHeight: 1.5, textAlign: "center" }}>
                                     {"On "}
                                     <strong>{unlockBy}</strong>
                                     {" you get back"}
@@ -302,6 +297,19 @@ class LockContainer extends React.Component {
                                         <AEUR amount={earnAmount} />
                                     </strong>
                                     {"."}
+                                </p>
+                                <p
+                                    style={{
+                                        color: theme.colors.darkRed,
+                                        fontWeight: "bold",
+                                        fontSize: 14,
+                                        margin: "20px auto 20px auto",
+                                        textAlign: "center",
+                                        width: "90%",
+                                        lineHeight: "120%"
+                                    }}
+                                >
+                                    A-EUR unlock only available after <br /> {unlockBy}
                                 </p>
                             </div>
                         )}
