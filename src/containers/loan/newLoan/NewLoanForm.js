0dccdff571e0a2401490eadd98b8fbaf48d1d6b6
Viktor Szathmáry <phraktle@gmail.com>
2019-05-08 14:31:58 +0200

    Form validation improvements (#615)

--- src/containers/loan/newLoan/NewLoanForm.js
+++ src/containers/loan/newLoan/NewLoanForm.js
@@ -310,9 +310,6 @@ class NewLoanForm extends React.Component {
                             inputmode="numeric"
                             step="any"
                             min="0"
-                            ref={input => {
-                                this.input = input;
-                            }}
                             disabled={submitting || !loanManager.isLoaded}
                             validate={[
                                 Validations.required,
@@ -407,6 +404,7 @@ const mapStateToProps = state => ({
 
 NewLoanForm = reduxForm({
     form: "NewLoanForm",
-    touchOnBlur: false
+    touchOnBlur: false,
+    touchOnChange: true
 })(NewLoanForm);
 export default connect(mapStateToProps)(NewLoanForm);
