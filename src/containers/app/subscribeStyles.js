8db3dbf9a2044bba7f24c6fece96b7716029d8ad
brigittaforrai <forraibrigi@gmail.com>
2019-05-14 16:30:00 +0200

    footer

--- src/containers/app/subscribeStyles.js
+++ src/containers/app/subscribeStyles.js
@@ -6,9 +6,17 @@ import { remCalc } from "styles/theme";
 
 export const StyledField = styled(Field)`
     color: ${theme.colors.opacLightGrey};
-    font-size: ${remCalc(12)} !important;
+    font-size: ${remCalc(16)} !important;
     font-weight: 200 !important;
     letter-spacing: 2.4px;
     line-height: ${remCalc(21)};
     min-width: 246px;
+    height: 50px;
+    max-width: 400px;
+    margin: 0 auto;
+    ::placeholder {
+        color: gray !important;
+        font-size: ${remCalc(16)} !important;
+        font-weight: 200 !important;
+    }
 `;
