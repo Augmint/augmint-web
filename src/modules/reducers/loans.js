48382264aa8dfd3cebd8b6fb8f128076aa6f1e46
szerintedmi <szerintedmi@gmail.com>
2019-05-16 22:14:18 +0200

    all loans to underthehood

--- src/modules/reducers/loans.js
+++ src/modules/reducers/loans.js
@@ -1,14 +1,19 @@
 /* Loans for one account
     TODO: consider selectors https://github.com/reactjs/reselect */
 import store from "modules/store";
-import { fetchLoansForAddressTx } from "modules/ethereum/loanTransactions";
+import { fetchLoansForAddressTx, fetchAllLoansTx } from "modules/ethereum/loanTransactions";
 
-export const LOANS_LOANLIST_REQUESTED = "loans/LOANS_LOANLIST_REQUESTED";
-export const LOANS_LOANLIST_RECEIVED = "loans/LOANS_LOANLIST_RECEIVED";
-export const LOANS_LOANLIST_ERROR = "loans/LOANS_LOANLIST_ERROR";
+export const LOANS_LOANLIST_REQUESTED = "loans/LOANLIST_REQUESTED";
+export const LOANS_LOANLIST_RECEIVED = "loans/LOANLIST_RECEIVED";
+export const LOANS_LOANLIST_ERROR = "loans/LOANLIST_ERROR";
+
+export const LOANS_FETCH_ALL_REQUESTED = "loans/FETCH_ALL_REQUESTED";
+export const LOANS_FETCH_ALL_RECEIVED = "loans/FETCH_ALL_RECEIVED";
+export const LOANS_FETCH_ALL_ERROR = "loans/FETCH_ALL_ERROR";
 
 const initialState = {
     loans: null,
+    allLoans: null,
     isLoading: false,
     isLoaded: false,
     loadError: null
@@ -30,6 +35,21 @@ export default (state = initialState, action) => {
                 loans: action.loans
             };
 
+        case LOANS_FETCH_ALL_REQUESTED:
+            return {
+                ...state,
+                error: null,
+                isLoading: true
+            };
+
+        case LOANS_FETCH_ALL_RECEIVED:
+            return {
+                ...state,
+                isLoading: false,
+                allLoans: action.result
+            };
+
+        case LOANS_FETCH_ALL_ERROR:
         case LOANS_LOANLIST_ERROR:
             return {
                 ...state,
@@ -41,6 +61,31 @@ export default (state = initialState, action) => {
             return state;
     }
 };
+
+export function fetchAllLoans() {
+    return async dispatch => {
+        dispatch({
+            type: LOANS_FETCH_ALL_REQUESTED
+        });
+
+        try {
+            const allLoans = await fetchAllLoansTx();
+
+            return dispatch({
+                type: LOANS_FETCH_ALL_RECEIVED,
+                result: allLoans
+            });
+        } catch (error) {
+            if (process.env.NODE_ENV !== "production") {
+                throw new Error(error);
+            }
+            return dispatch({
+                type: LOANS_FETCH_ALL_ERROR,
+                error: error
+            });
+        }
+    };
+}
 
 export function fetchLoansForAddress(userAccount) {
     return async dispatch => {
