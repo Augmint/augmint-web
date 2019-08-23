acb391e219aa24f9dcfc364217bebbe6b8201001
Treer Zoltán <treer@euedge.com>
2019-08-23 13:46:36 +0200

    show eth balance for loanmanagers

--- src/containers/underthehood/components/LoanManagerInfo.js
+++ src/containers/underthehood/components/LoanManagerInfo.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { utils } from "@augmint/js";
+import { utils, Tokens, Wei } from "@augmint/js";
 import { Pblock, Psegment } from "components/PageLayout";
 import Button from "components/augmint-ui/button";
-import userBalances from "modules/reducers/userBalances";
-import legacyBalances from "modules/reducers/legacyBalances";
 
 const blockHeaderStyle = { fontSize: "1.5em", fontWeight: 300 };
 
 class LoanManagerInfo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isLoaded: false, loanManagers: [], tokenAdresses: {}, loanProducts: {}, loans: {} };
+        this.state = {
+            isLoaded: false,
+            loanManagers: [],
+            tokenAdresses: {},
+            loanProducts: {},
+            loans: {},
+            ethBalances: {}
+        };
         this.handleRefreshClick = this.handleRefreshClick.bind(this);
     }
 
     async refresh() {
-        const augmint = this.props.augmint;
+        const augmint = this.props.web3Connect.augmint;
         const isLoaded = true;
         const loanManagers = (augmint && augmint.getAllLoanManagers()) || [];
         const tokenAddresses = new Map(
@@ -27,7 +32,13 @@ class LoanManagerInfo extends React.Component {
             await Promise.all(loanManagers.map(async m => [m.address, await m.getAllProducts()]))
         );
         const loans = new Map(await Promise.all(loanManagers.map(async m => [m.address, await m.getAllLoans()])));
-        this.setState({ isLoaded, loanManagers, tokenAddresses, loanProducts, loans });
+
+        const web3 = this.props.web3Connect.web3Instance;
+        const ethBalances = new Map(
+            await Promise.all(loanManagers.map(async m => [m.address, Wei.parse(await web3.eth.getBalance(m.address))]))
+        );
+
+        this.setState({ isLoaded, loanManagers, tokenAddresses, loanProducts, loans, ethBalances });
     }
 
     handleRefreshClick(e) {
@@ -47,7 +58,7 @@ class LoanManagerInfo extends React.Component {
                 </Pblock>
             );
         }
-        if (!this.props.augmint) {
+        if (!this.props.web3Connect || !this.props.web3Connect.augmint) {
             return (
                 <Pblock header="LoanManagers" headerStyle={blockHeaderStyle}>
                     <p>Error: No augmint environment</p>
@@ -66,7 +77,7 @@ class LoanManagerInfo extends React.Component {
         const abiHashes = new Map(
             this.state.loanManagers.map(m => [
                 m.address,
-                this.props.augmint.deployedEnvironment.getAbiHash("LoanManager", m.address)
+                this.props.web3Connect.augmint.deployedEnvironment.getAbiHash("LoanManager", m.address)
             ])
         );
         const abis = new Map(this.state.loanManagers.map(m => [m.address, m.instance._jsonInterface]));
@@ -84,10 +95,20 @@ class LoanManagerInfo extends React.Component {
             <Psegment key={m}>
                 Address: <strong>{m}</strong>
                 <br />
+                <br />
                 Products: {activeProdCounts.get(m)} active, {disabledProdCounts.get(m)} disabled
                 <br />
                 Loans: {loanCounts.get(m)} total, {repaidLoanCounts.get(m)} repaid, {collectedLoanCounts.get(m)}{" "}
                 collected, {collectableLoanCounts.get(m)} collectable, {repayableLoanCounts.get(m)} repayable
+                <br />
+                ETH balance:{" "}
+                <strong>
+                    {this.state.ethBalances
+                        .get(m)
+                        .toNumber()
+                        .toFixed(6) + " ETH"}
+                </strong>
+                {" (" + this.state.ethBalances.get(m).toString() + " wei)"}
                 <br />
                 <br />
                 Token: <strong>{this.state.tokenAddresses.get(m)}</strong>
@@ -119,8 +140,7 @@ class LoanManagerInfo extends React.Component {
 const countOf = (map, filter) => utils.mapMap(map, val => val.filter(filter).length);
 
 const mapStateToProps = state => ({
-    augmint: state.web3Connect.augmint,
-    allLoans: state.loans.allLoans
+    web3Connect: state.web3Connect
 });
 
 export default connect(mapStateToProps)(LoanManagerInfo);
